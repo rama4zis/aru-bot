2bf9eefb1e1ebcbcfed2137717ff1e2714015f1c
rama4zis <rama.4ditya3@gmail.com>
2023-07-03 18:04:24 +0700

    Fix menu and add some alert

--- src/controller/MenuController.ts
+++ src/controller/MenuController.ts
@@ -7,7 +7,7 @@ class MenuController extends MessageHandler {
         this.client.sendMessage(this.msgData.from, 
         "Menu: \n\n" +
         "1. Sticker: Kirim gambar dengan caption '.s' \n" +
-        "2. Sticker Gif: Kirim video dengan caption '.sg'" +
+        "2. Sticker Gif: Kirim video dengan caption '.sg'\n" +
         "3. Download Youtube Mp3: Kirim pesan dengan format '.ytmp3 <url>' \n" +
         "\n\nGunakan caption tanpa tanda kutip"
         )
--- src/controller/stickerController.ts
+++ src/controller/stickerController.ts
@@ -19,13 +19,17 @@ class StickerController extends MessageHandler {
     async stickerGif() {
         if (this.msgData.hasMedia && this.msgData.type === 'video') {
             try {
-
                 const media = await this.msgData.downloadMedia();
+                if(media === undefined) {
+                    this.client.sendMessage(this.msgData.from, 'Maaf, sticker gif tidak bisa dikirim');
+                    return;
+                }
                 this.client.sendMessage(this.msgData.from, media, {
                     sendMediaAsSticker: true,
                     stickerAuthor: '+62 895-4139-26068',
                     stickerName: 'Arisu Chatbot'
                 })
+                console.log('Sticker Gif sent');
             } catch (error) {
                 console.log(error);
             }
