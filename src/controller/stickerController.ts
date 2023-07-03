b2a8eea90ba00ad7a4339275c06a6acc7b19480f
rama4zis <rama.4ditya3@gmail.com>
2023-07-03 20:50:10 +0700

    export default

--- src/controller/stickerController.ts
+++ src/controller/stickerController.ts
@@ -1,7 +1,7 @@
 import MessageHandler from "../handler/messageHandler";
 
 
-class StickerController extends MessageHandler {
+export default class StickerController extends MessageHandler {
 
     async stickerNormal() {
         if (this.msgData.hasMedia && this.msgData.type === 'image') {
@@ -37,5 +37,3 @@ class StickerController extends MessageHandler {
     }
 
 }
-
-export = StickerController;
