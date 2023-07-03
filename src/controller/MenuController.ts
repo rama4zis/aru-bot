e4d95e173af20864c7290d3f21ff55e3f8c593f4
rama4zis <rama.4ditya3@gmail.com>
2023-07-03 12:07:16 +0700

    Note for menu

--- src/controller/MenuController.ts
+++ src/controller/MenuController.ts
@@ -5,9 +5,10 @@ class MenuController extends MessageHandler {
     async menu() {
 
         this.client.sendMessage(this.msgData.from, 
-        "Menu: \n" +
+        "Menu: \n\n" +
         "1. Sticker: Kirim gambar dengan caption '.s' \n" +
-        "2. Sticker Gif: Kirim video dengan caption '.sg'"
+        "2. Sticker Gif: Kirim video dengan caption '.sg'" +
+        "\n\nGunakan caption tanpa tanda kutip"
         )
 
     }
